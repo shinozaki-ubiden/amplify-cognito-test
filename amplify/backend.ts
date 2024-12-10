fbf7ccfd66d47f724c718617f45ea5efce5d05df
shinozaki-ubiden <yuma.shinozaki@ubiden.com>
2024-12-11 01:15:03 +0900

    disabled self sign-up

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,14 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+// disable self sign-up
+// https://github.com/aws-amplify/amplify-backend/issues/1656
+const { cfnUserPool } = backend.auth.resources.cfnResources;
+cfnUserPool.adminCreateUserConfig = {
+  allowAdminCreateUserOnly: true,
+}
